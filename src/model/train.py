60cda57a24d0f785a67bf44e3f21e3eed5c353ec
Nurbolat <nurbolat.bazarbek@nu.edu.kz>
2024-04-22 21:40:33 +0200

    updated lint issues

--- src/model/train.py
+++ src/model/train.py
@@ -3,7 +3,6 @@ import argparse
 import mlflow
 import glob
 import os
-import numpy as np 
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
@@ -12,28 +11,25 @@ from sklearn.model_selection import train_test_split
 def main(args):
     # TO DO: enable autologging
     mlflow.autolog()
-
     # read data
-    df = get_csvs_df(args.training_data)
-
+    df = get_csvs_df(args.training_data) 
     # split data
     X_train, X_test, y_train, y_test = split_data(df)
-
     # train model
     train_model(args.reg_rate, X_train, X_test, y_train, y_test)
 
 
 def get_csvs_df(path):
-    if not os.path.exists(path):
+    if not os.path.exists(path): 
         raise RuntimeError(f"Cannot use non-existent path provided: {path}")
     csv_files = glob.glob(f"{path}/*.csv")
-    if not csv_files:
+    if not csv_files: 
         raise RuntimeError(f"No CSV files found in provided data path: {path}")
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
 # TO DO: add function to split data
-def split_data(df:pd.DataFrame):
+def split_data(df: pd.DataFrame):
     X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
     return train_test_split(X, y, test_size=0.30, random_state=0)
 
@@ -57,9 +53,6 @@ def parse_args():
 
     # return args
     return args
-
-
-
 
 
 # run script
